cf60cb06898c5e6808aa36b64e82edf0f2b2087e
Jason Teraani <jason.teraani@gmail.com>
2025-07-26 02:48:18 +0000

    percebi que ainda não coube os 4 jogadores da zaga no campo. Talvez dimi

--- src/components/lineup/pitch.tsx
+++ src/components/lineup/pitch.tsx
@@ -35,7 +35,7 @@ export default function Pitch({ children, modality }: PitchProps) {
     <div className={cn("pitch w-full h-[600px] rounded-lg mx-auto flex flex-col justify-between items-stretch relative overflow-hidden border-2 border-white/30", className)}>
       {/* Field Lines */}
       <div className="absolute top-1/2 left-0 right-0 h-0.5 bg-white/30" />
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-32 h-32 border-2 border-white/30 rounded-full" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-28 h-28 border-2 border-white/30 rounded-full" />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2 h-2 bg-white/30 rounded-full" />
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[40%] h-16 border-2 border-b-2 border-t-0 border-white/30 rounded-b-xl" />
       <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[40%] h-16 border-2 border-t-2 border-b-0 border-white/30 rounded-t-xl" />
