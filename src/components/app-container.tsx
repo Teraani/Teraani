26d77ac63a29264af05d28bfee6657459d94d300
Jason Teraani <jason.teraani@gmail.com>
2025-08-02 00:24:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -1,5 +1,4 @@
 
-
 "use client";
 
 import { useState, useMemo, useEffect } from 'react';
@@ -1061,5 +1060,9 @@ export default function AppContainer() {
     </div>
   );
 }
+
+    
+
+    
 
     
